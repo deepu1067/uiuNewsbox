62a6de4cf07da7921e5a7b8fcd7a4de92877bf60
deepu1067 <dshahadat10@gmail.com>
2022-08-28 22:14:16 +0600

    edit

--- pages/sqlCommands/connectDb.php
+++ pages/sqlCommands/connectDb.php
@@ -4,33 +4,4 @@
     $pass = '';
 
     $connect = mysqli_connect($host, $user, $pass);
-
-    try{
-        $db = mysqli_select_db($connect, "uiuNewsBox");
-    }
-    catch(Exception $e){
-        $db = 0;
-    }
-
-    if($db == 0){
-        mysqli_query($connect, "create database uiuNewsBox;");
-    }
-    else{
-        echo "found";
-        mysqli_query($connect, "create table admin(
-                                    id int,
-                                    first_name varchar(50) not null,
-                                    last_name varchar(50) not null,
-                                    email varchar(100),
-                                    phone_number varchar(50),
-                                    primary key(id)
-                                )
-                                create table admin_address(
-                                    address varchar(100) not null,
-                                    id int,
-                                    foreign key(id) references admin(id)
-                                )");
-    }
-
-    
 ?>
