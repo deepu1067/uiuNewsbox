3ace299f8524f97d2ea24d2f5bb6f1d817400305
deepu1067 <dshahadat10@gmail.com>
2022-08-29 06:40:26 +0600

    edit

--- pages/connectDb.php
+++ pages/connectDb.php
@@ -7,5 +7,5 @@ use function PHPSTORM_META\type;
     $pass = '';
 
     $sql = mysqli_connect($host, $user, $pass); //server connect
-    $db = mysqli_select_db($sql, "hr_schema"); //database select
+    $db = mysqli_select_db($sql, "uiuNewsBox"); //database select
 ?>
