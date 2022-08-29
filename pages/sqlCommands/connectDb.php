0ccd4ececacf5da788566e814438898ac9a8b131
deepu1067 <dshahadat10@gmail.com>
2022-08-29 06:31:59 +0600

    login

--- pages/sqlCommands/connectDb.php
+++ pages/sqlCommands/connectDb.php
@@ -1,7 +1,11 @@
 <?php
+
+use function PHPSTORM_META\type;
+
     $host = "localhost:3306";
     $user = 'root';
     $pass = '';
 
-    $connect = mysqli_connect($host, $user, $pass);
+    $sql = mysqli_connect($host, $user, $pass); //server connect
+    $db = mysqli_select_db($sql, "hr_schema"); //database select
 ?>
