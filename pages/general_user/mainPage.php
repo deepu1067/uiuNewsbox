6438609bb3ebd25957982bdf6fc5cfecd1b0d720
deepu1067 <dshahadat10@gmail.com>
2022-09-11 08:39:43 +0600

    edit

--- pages/general_user/mainPage.php
+++ pages/general_user/mainPage.php
@@ -23,17 +23,17 @@ include 'room.php';
 
     <div class="d-flex align-items-start">
         <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-            <button class="nav-link " id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button>
+            <button class="nav-link active " id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button>
 
             <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</button>
 
             <button class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Group</button>
 
-            <button class="nav-link active" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Joined</button>
+            <button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Joined</button>
         </div>
 
         <div class="tab-content" id="v-pills-tabContent" style="width: 85%!important;">
-            <div class="tab-pane fade" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">
+            <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">
                 <!-- HOME -->
                 <section class="mt-2" id="form">
                     <?php while ($row = mysqli_fetch_assoc($query)) : ?>
@@ -181,7 +181,7 @@ include 'room.php';
                             <div class="card-title fw-bold text-uppercase text-center"><?php echo $srow["forum_name"]; ?></div>
                             <div class="card-body">
                                 <?php if (!$joined) : ?>
-                                    <p class="m-0 text-center">Want be part of discussion room? </p>
+                                    <p class="m-0 text-center">Want to be part of discussion room? </p>
                                     <form action="join.php" class="d-flex justify-content-center align-items-center" method="post">
                                         <input type="hidden" name="room_id" <?php echo "value='{$srow["id"]}'"; ?>>
                                         <button class="btn btn-uiu" type="submit">Join</button>
@@ -196,7 +196,7 @@ include 'room.php';
                 <?php endwhile ?>
             </div>
 
-            <div class="tab-pane fade show active" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab" tabindex="0">
+            <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab" tabindex="0">
                 <!-- joined -->
                 <?php if (mysqli_num_rows($joined_q) == 0) : ?>
                     <h2 class="text-center" >Please join a forum discussion room first</h2>
