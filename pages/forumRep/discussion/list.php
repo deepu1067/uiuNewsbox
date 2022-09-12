d5d5ad29ba7f19e69ee29fb048419a33e95e1cf4
deepu1067 <dshahadat3@gmail.com>
2022-09-12 18:53:14 +0600

    edit

--- pages/forumRep/discussion/list.php
+++ pages/forumRep/discussion/list.php
@@ -1,7 +1,7 @@
 <?php 
     include '../../sqlCommands/connectDb.php';
 
-    $str = "select r.id as room_id , r.forum_name forum_name , r.forumRep_id forumRep_id , u.users_id users_id , u.approve, concat(g.first_name, ' ', g.last_name) 'name', g.email email from room r join users u on r.id = u.room_id join general_user g on u.users_id=g.id where r.forumRep_id = {$_SESSION["id"]} and u.approve = 0";
+    $str = "select r.id as room_id , r.forum_name forum_name , r.forumRep_id forumRep_id , u.users_id users_id , u.approve approve, concat(g.first_name, ' ', g.last_name) 'name', g.email email from room r join users u on r.id = u.room_id join general_user g on u.users_id=g.id where r.forumRep_id = {$_SESSION["id"]} and u.approve = 0";
 
     $s_query = mysqli_query($sql, $str);
 
--- pages/forumRep/discussion/room.php
+++ pages/forumRep/discussion/room.php
@@ -18,7 +18,7 @@ include "list.php";
         <img src="../../../assets/img/ForOrangeBg.png" alt="logo" class="img-fluid pt-1 pb-1">
         <div class="d-flex flex-column justify-content-center align-items-center p-1">
             <p class="m-0 text-uppercase mb-1 p-1 fw-bold   " style="border: 1px solid white; border-radius:10px ;"><?php echo show($_SESSION["id"]); ?></p>
-            <a href="../login/logout.php" class="btn btn-uiu text-uppercase">logout</a>
+            <a href="../../login/logout.php" class="btn btn-uiu text-uppercase">logout</a>
         </div>
     </nav>
 
